46a233a86f5bdfd9dd09c69559d867911a57f7b8
Abdullah-jameel <abdullahjamilrao@gmail.com>
2023-10-10 17:05:34 +0500

    Added variables and wrote first 4 lines of code

---
+++ airDistance.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(int argc, char const *argv[])
+{
+    float latitude1, latitude2, longitude1, longitude2, d, R = 6371;
+    printf("Enter the latitude of location A");
+    scanf("%f",&latitude1);
+    ptintf("Enter the longitude of location A");
+    scanf("%f",& longitude1);
+    return 0;
+}
