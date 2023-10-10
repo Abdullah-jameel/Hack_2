138adbce77d35ff4820c9489f9d1a65aa803da4c
Abdullah-jameel <abdullahjamilrao@gmail.com>
2023-10-10 17:13:36 +0500

    Eedited 1st & 2nd printf statement and added 3rd printf statement

--- airDistance.c
+++ airDistance.c
@@ -4,9 +4,10 @@
 int main(int argc, char const *argv[])
 {
     float latitude1, latitude2, longitude1, longitude2, d, R = 6371;
-    printf("Enter the latitude of location A");
+    printf("Enter the latitude of location A: ");
     scanf("%f",&latitude1);
-    ptintf("Enter the longitude of location A");
+    printf("Enter the longitude of location A: ");
     scanf("%f",& longitude1);
+    printf("Origin:  \t (%f, %f)",latitude1,longitude1);
     return 0;
 }
