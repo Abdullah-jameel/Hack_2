e7e96a262e903217b8dc352ddac8146e6be75e0f
Abdullah-jameel <abdullahjamilrao@gmail.com>
2023-10-10 17:27:31 +0500

    wrote line 16th and edited line 11th of code

--- airDistance.c
+++ airDistance.c
@@ -8,10 +8,11 @@ int main(int argc, char const *argv[])
     scanf("%f",&latitude1);
     printf("Enter the longitude of location A: ");
     scanf("%f",& longitude1);
-    printf("Origin:  \t (%f, %f)",latitude1,longitude1);
+    printf("Origin:  \t (%f, %f) \n", latitude1, longitude1);
     printf("Enter the latitude of location B: ");
     scanf("%f",&latitude2);
     printf("enter the longitude of location B: ");
     scanf("%f",&longitude2);
+    printf("Distination: \t (%f, %f) \n", latitude2, longitude2);
     return 0;
 }
